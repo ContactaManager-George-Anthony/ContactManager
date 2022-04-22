b21606964744b97de485ac07770208c292bfd6f0
George Puryear <gm.puryear@yahoo.com>
2022-04-22 12:02:37 -0500

    created selection menu, getters and setters for contacts

--- src/Contacts/Contact.java
+++ src/Contacts/Contact.java
@@ -1,2 +1,31 @@
+package Contacts;
+
 public class Contact {
+    // fields
+    private String name;
+
+    private Long phoneNum;
+
+    // constructor
+    public Contact(String name, Long phoneNum) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(Long phoneNum) {
+        this.phoneNum = phoneNum;
+    }
 }
