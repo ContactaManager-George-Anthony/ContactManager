743e328b2875dc56070ef36fa28251338a4a484e
George Puryear <gm.puryear@yahoo.com>
2022-04-25 09:22:15 -0500

    organized classes and code

---
+++ src/Contacts/Hashmap.java
@@ -0,0 +1,13 @@
+package Contacts;
+
+import java.util.HashMap;
+
+public class Hashmap {
+    public static HashMap<Integer, Contact> createHashMap() {
+        HashMap<Integer, Contact> contacts = new HashMap<>();
+        return contacts;
+    }
+    // create hashmap object
+
+
+}
