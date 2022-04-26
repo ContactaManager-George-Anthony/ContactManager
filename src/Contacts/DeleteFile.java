87b8f95a3db012843b715e0e3bbf383713e14678
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-04-26 09:32:15 -0500

    Adding Delete Method

--- src/Contacts/DeleteFile.java
+++ src/Contacts/DeleteFile.java
@@ -1,14 +1,62 @@
 package Contacts;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-import java.io.File;  // Import the File class
 
 public class DeleteFile {
-    public static void main(String[] args) {
-        File myObj = new File("filename.txt");
-        if (myObj.delete()) {
-            System.out.println("Deleted the file: " + myObj.getName());
-        } else {
-            System.out.println("Failed to delete the file.");
+    public static void deleteContact(List<Contact> contactList) {
+        Path ContactsPath = Paths.get("data", "contactsInfo.json");
+        FileHelper.tryReadFile(ContactsPath);
+        System.out.println("4. Delete contacts by name and/or phone number. ");
+        System.out.print(" Name or Number: ");
+        Scanner myScanner = new Scanner(System.in);
+        String searchedName = myScanner.nextLine();
+        for (Contact contact : contactList){
+            if (searchedName.equalsIgnoreCase(contact.getFirstName() +" "+ contact.getLastName())) {
+                contactList.remove(contact);
+            }
         }
+        System.out.println("Name         Phone#");
+        System.out.println("-----------------------------------");
+        for (Contact contact : contactList) {
+            System.out.println(
+                    contact.getFirstName() + "" +
+                            " " +
+                            contact.getLastName() + " | " +
+                            contact.getPhoneNum());
+        }
+
+        }
+
+//        Path ContactsPath = Paths.get("data", "contactsInfo.json");
+//        List<String> Personlist;
+//        try {
+//            Personlist = Files.readAllLines(ContactsPath);
+//            List<String> newList = new ArrayList<>();
+//            for (String person : Personlist) {
+//                if (person.toLowerCase().contains(searchedName)) {
+//                    continue;
+//                }
+//                newList.add(person);
+//            }
+//            for (String name : newList) {
+//                System.out.println(name);
+//            }
+//            Files.write(Paths.get("data", "contactsInfo.json"), newList);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//
+//        }
+//    }
+
+    public static void exit() {
+        System.out.println("Goodbye");
+        System.exit(0);
+
     }
 }
